295aa90857dd921aa28ea35bb614b62dbe93b7a7
lucianobs1 <lucianotetsu@gmail.com>
2023-02-23 10:30:00 -0300

    chore: update globalStyles and defaultTheme with new Colors and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { GlobalStyles } from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 
@@ -5,6 +6,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
+
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
