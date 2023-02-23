82b9d75864a2af2ae972d06e9301a4a60b33070c
lucianobs1 <lucianotetsu@gmail.com>
2023-02-23 10:51:28 -0300

    feat: add react-router-dom and config app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { GlobalStyles } from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './components/Router';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyles />
     </ThemeProvider>
